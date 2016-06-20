45aa913134b8f7d1bc68560c156bf72fc6d9c1e2
xujixiao <1107313740@qq.com>
2016-06-20 17:35:58 +0800

    添加log utils的工具类

---
+++ app/src/main/java/com/example/xujixiao/myapplication/common/adapter/utils/TLog.java
@@ -0,0 +1,37 @@
+package com.example.xujixiao.myapplication.common.adapter.utils;
+
+import android.util.Log;
+
+import com.example.xujixiao.myapplication.BuildConfig;
+
+/**
+ * 工具类实现输出日志信息等
+ */
+public class TLog {
+    public static String LOG_TAG = "xujixiao";
+    public static boolean DEBUG = BuildConfig.DEBUG;
+
+    public TLog() {
+    }
+
+    public static void error(String log) {
+        if (DEBUG && log != null)
+            Log.e(LOG_TAG, "" + log);
+    }
+
+    public static void log(String log) {
+        if (DEBUG && log != null) {
+            Log.e(LOG_TAG, log);
+        }
+    }
+
+    public static void log(String tag, String log) {
+        if (DEBUG && log != null)
+            Log.i(tag, log);
+    }
+
+    public static void logException(Exception e) {
+        if (DEBUG && e != null) {
+        }
+    }
+}
